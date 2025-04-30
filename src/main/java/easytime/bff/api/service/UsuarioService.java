bd70ff9b2c1a1a03a48c40542e6a7647e6d065af
vividsantos <viviane.familia834149@gmail.com>
2025-04-30 13:08:25 -0300

    fix: Rota de cadastro funcionando sem token

--- src/main/java/easytime/bff/api/service/UsuarioService.java
+++ src/main/java/easytime/bff/api/service/UsuarioService.java
@@ -20,23 +20,26 @@ import static org.springframework.http.HttpMethod.*;
 public class UsuarioService {
 
     public ResponseEntity<Object> criarUsuario(UsuarioDto dto, HttpServletRequest request) {
-        try{
+        try {
             RestTemplate restTemplate = new RestTemplate();
-            // String url = "https://70b9bf47-dcb1-46e9-9886-1110d671967d-00-1upha6j38mgjy.riker.replit.dev:8080/users/create";
             String url = "http://localhost:8080/users/create";
             HttpHeaders headers = new HttpHeaders();
+
             headers.set("Content-Type", "application/json");
             Enumeration<String> headerNames = request.getHeaderNames();
+
             while (headerNames.hasMoreElements()) {
                 String headerName = headerNames.nextElement();
                 headers.add(headerName, request.getHeader(headerName));
             }
 
+            if (headers.containsKey("Content-Length")) {
+                headers.remove("Content-Length");
+            }
+
             HttpEntity<UsuarioDto> entity = new HttpEntity<>(dto, headers);
+            return restTemplate.exchange(url, HttpMethod.PUT, entity, Object.class);
 
-            ResponseEntity<Object> response = restTemplate.exchange(url, PUT, entity, Object.class);
-
-            return response;
         } catch (HttpClientErrorException e) {
             throw new RuntimeException("Erro na requisição: " + e.getResponseBodyAsString(), e);
         } catch (HttpMessageNotReadableException e) {
@@ -45,5 +48,36 @@ public class UsuarioService {
             throw new RuntimeException("Erro ao criar usuário: " + e.getMessage(), e);
         }
     }
+
+
+//    public ResponseEntity<Object> criarUsuario(UsuarioDto dto, HttpServletRequest request) {
+//        try{
+//            RestTemplate restTemplate = new RestTemplate();
+//            // String url = "https://70b9bf47-dcb1-46e9-9886-1110d671967d-00-1upha6j38mgjy.riker.replit.dev:8080/users/create";
+//            String url = "http://localhost:8080/users/create";
+//
+//            // Algum problema no header
+//
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.set("Content-Type", "application/json");
+//            Enumeration<String> headerNames = request.getHeaderNames();
+//            while (headerNames.hasMoreElements()) {
+//                String headerName = headerNames.nextElement();
+//                headers.add(headerName, request.getHeader(headerName));
+//            }
+//
+//            HttpEntity<UsuarioDto> entity = new HttpEntity<>(dto, headers);
+//
+//            ResponseEntity<Object> response = restTemplate.exchange(url, PUT, entity, Object.class);
+//
+//            return response;
+//        } catch (HttpClientErrorException e) {
+//            throw new RuntimeException("Erro na requisição: " + e.getResponseBodyAsString(), e);
+//        } catch (HttpMessageNotReadableException e) {
+//            throw new RuntimeException("Formato de JSON inválido. Verifique o corpo da requisição.", e);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Erro ao criar usuário: " + e.getMessage(), e);
+//        }
+//    }
 
 }
