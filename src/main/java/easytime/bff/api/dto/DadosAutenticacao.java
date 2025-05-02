f084efbca65cc3b7052de17a87a4443db4ce74b5
vividsantos <viviane.familia834149@gmail.com>
2025-05-02 13:10:25 -0300

    fix: Melhor descriçãos dos DTOs

--- src/main/java/easytime/bff/api/dto/DadosAutenticacao.java
+++ src/main/java/easytime/bff/api/dto/DadosAutenticacao.java
@@ -1,4 +1,12 @@
 package easytime.bff.api.dto;
 
-public record DadosAutenticacao (String login, String senha) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Dados para autenticação do usuário")
+public record DadosAutenticacao (
+        @Schema(description = "Login do usuário", example = "usuario123")
+        String login,
+        @Schema(description = "Senha do usuário", example = "senha#123")
+        String senha
+) {
 }
