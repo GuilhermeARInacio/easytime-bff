98019d0da59dd332a2f8c4fc97de8c5edf573855
Easytime-App <easytime123.app@gmail.com>
2025-05-09 18:08:09 +0000

    fix: Erros do código mais claros

--- src/main/java/easytime/bff/api/util/ExceptionHandlerUtil.java
+++ src/main/java/easytime/bff/api/util/ExceptionHandlerUtil.java
@@ -29,8 +29,8 @@ public class ExceptionHandlerUtil {
 
     private static ResponseEntity<?> tratarHttpClientErrorException(HttpClientErrorException e, Logger logger) {
         var statusCode = e.getStatusCode();
-        var mensagem401 = e.getMessage().contains("codigo") ? "Tente enviar um novo código, ocorreu um erro pois o código está inválido." : "Usuário não autorizado.";
-        var mensagem404 = e.getMessage().contains("codigo") ? "Tente enviar um novo código, ocorreu um erro pois o código está inválido." : "Serviço não encontrado.";
+        var mensagem401 = e.getMessage().contains("código") ? "Tente enviar um novo código, ocorreu um erro pois o código está inválido." : "Usuário não autorizado.";
+        var mensagem404 = e.getMessage().contains("código") ? "Tente enviar um novo código, ocorreu um erro pois o código está inválido." : "Serviço não encontrado.";
         logger.warn("Erro HTTP: {} - {}", statusCode.value(), e.getMessage());
 
         if (statusCode.is4xxClientError()) {
