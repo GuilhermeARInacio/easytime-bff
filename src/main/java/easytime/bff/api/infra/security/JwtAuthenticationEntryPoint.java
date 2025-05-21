1d667cf057e938e27d02c5e4da80d5df786b1667
vividsantos <viviane.familia834149@gmail.com>
2025-05-21 09:08:43 -0300

    fix: Deletar ponto

---
+++ src/main/java/easytime/bff/api/infra/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package easytime.bff.api.infra.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("text/plain");
+        response.getWriter().write("Acesso negado. Faça login para continuar.");
+    }
+}
