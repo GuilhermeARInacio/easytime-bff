ae3dbe423fdbc6acd4b3ce58bfd9f3f273a68d15
vividsantos <viviane.familia834149@gmail.com>
2025-05-29 13:20:31 -0300

    fix: Login deve ter pelo menos uma letra, e ajustes de testes

--- src/main/java/easytime/bff/api/validacoes/login/ValidacaoUsuarioLogin.java
+++ src/main/java/easytime/bff/api/validacoes/login/ValidacaoUsuarioLogin.java
@@ -9,8 +9,8 @@ public class ValidacaoUsuarioLogin implements ValidacoesLogin {
         if(dto.login().isEmpty() || dto.login().isBlank()) {
             throw new RuntimeException("O usuário não pode ser vazio");
         }
-        if(!dto.login().matches("^[a-zA-Z0-9._]+$")){
-            throw new RuntimeException("O usuário só pode conter letras, números, pontos e underlines");
+        if(!dto.login().matches("^(?=.*[a-zA-Z])[a-zA-Z0-9._]+$")){
+            throw new IllegalArgumentException("O usuário deve ter pelo menos uma letra, pode ter números, pontos e underlines");
         }
     }
 }
